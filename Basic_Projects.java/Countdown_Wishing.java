4c1851748903e9205cf62a57c020322a2c0479c1
Chandu <chanduthanuku24@gmail.com>
2025-07-13 13:47:11 +0530

    Create Countdown_Wishing.java

---
+++ Basic_Projects.java/Countdown_Wishing.java
@@ -0,0 +1,10 @@
+ class appy_New_Year {
+    public static void main(String args[]) throws InterruptedException{
+        for(int i=5;i>0;i--){
+            System.out.println(i);
+            Thread.sleep(700);
+        }
+        System.out.println("Happy New Year");
+    }
+}
+// Used in Advance Java;
